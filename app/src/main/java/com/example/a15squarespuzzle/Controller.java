a3282f3bfb2b7a9f6e3e3d78a9cc8793d15acaba
Harrison-Winters <83734330+Harrison-Winters@users.noreply.github.com>
2021-09-20 16:50:12 -0700

    Created Controller, Model

---
+++ app/src/main/java/com/example/a15squarespuzzle/Controller.java
@@ -0,0 +1,6 @@
+package com.example.a15squarespuzzle;
+
+public class Controller {
+
+
+}
---
+++ app/src/main/java/com/example/a15squarespuzzle/Model.java
@@ -0,0 +1,20 @@
+package com.example.a15squarespuzzle;
+
+public class Model {
+    public String tile1;
+    public String tile2;
+    public String tile3;
+    public String tile4;
+    public String tile5;
+    public String tile6;
+    public String tile7;
+    public String tile8;
+    public String tile9;
+    public String tile10;
+    public String tile11;
+    public String tile12;
+    public String tile13;
+    public String tile14;
+    public String tile15;
+    public String tile16;
+}
