fdbdaa4224c4c07645524ff91231d2183a2314e6
Harrison-Winters <83734330+Harrison-Winters@users.noreply.github.com>
2021-09-20 17:22:01 -0700

    "Found" Textviews and assigned their variables

--- app/src/main/java/com/example/a15squarespuzzle/MainActivity.java
+++ app/src/main/java/com/example/a15squarespuzzle/MainActivity.java
@@ -3,8 +3,29 @@ package com.example.a15squarespuzzle;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    //Find TextViews
+    TextView Col0Row0 = (TextView)findViewById(R.id.Col0Row0);
+    TextView Col1Row0 = (TextView)findViewById(R.id.Col1Row0);
+    TextView Col2Row0 = (TextView)findViewById(R.id.Col2Row0);
+    TextView Col3Row0 = (TextView)findViewById(R.id.Col3Row0);
+    TextView Col0Row1 = (TextView)findViewById(R.id.Col0Row1);
+    TextView Col1Row1 = (TextView)findViewById(R.id.Col1Row1);
+    TextView Col2Row1 = (TextView)findViewById(R.id.Col2Row1);
+    TextView Col3Row1 = (TextView)findViewById(R.id.Col3Row1);
+    TextView Col0Row2 = (TextView)findViewById(R.id.Col0Row2);
+    TextView Col1Row2 = (TextView)findViewById(R.id.Col1Row2);
+    TextView Col2Row2 = (TextView)findViewById(R.id.Col2Row2);
+    TextView Col3Row2 = (TextView)findViewById(R.id.Col3Row2);
+    TextView Col0Row3 = (TextView)findViewById(R.id.Col0Row3);
+    TextView Col1Row3 = (TextView)findViewById(R.id.Col1Row3);
+    TextView Col2Row3 = (TextView)findViewById(R.id.Col2Row3);
+    TextView Col3Row3 = (TextView)findViewById(R.id.Col3Row3);
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
